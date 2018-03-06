76ee151dedf10d263130f4761ab489eb4b765d0a
chethan1992 <chethannb92@gmail.com>
2018-03-06 13:04:18 +0530

    second

--- Bufferedwriter.java
+++ Bufferedwriter.java
@@ -8,16 +8,19 @@ public class Bufferedwriter {
 	
 	public static void main(String[] args) throws Exception {
 	
-		//File f=new File("D:filewriter.txt")  ;
+		
 		FileWriter fw=new  FileWriter("f:\\filewriter.txt");
 		//BufferedWriter bw=new BufferedWriter(new FileWriter("D:\\filewriter.txt"));
 		BufferedWriter bw=new BufferedWriter(fw);
 
 		
 		bw.write(97);
+
 		bw.newLine();
+
 		bw.write("fchethan");
 		bw.newLine();
+
 		char[]ch={'g','h'};
 		bw.write(ch);
 		bw.flush();
